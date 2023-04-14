95ca3676960521317a8a0d4bbd1fd0332b6e7ed3
ShotaKhakhishvili <shotakhakhishvili@gmail.com>
2023-04-14 22:42:39 +0400

    Clamping Correction
    previous update had some problems with spacings...

--- Codes/PID.c
+++ Codes/PID.c
@@ -40,19 +40,19 @@ void PID_init(PID* pid, float Kp_val, float Ki_val, float Kd_val, float Kn_val, 
 {
 	// Miscems Shesabamis Koepicientebs Funqciashi Gadacemul Mnishvnelobebs
 
-	pid->Kp = Kp_val;
-	pid->Ki = Ki_val;
-	pid->Kd = Kd_val;
-	pid->Kn = Kn_val;
+		pid->Kp = Kp_val;
+		pid->Ki = Ki_val;
+		pid->Kd = Kd_val;
+		pid->Kn = Kn_val;
 
-	pid->outLimMax = outLimMax_val;
-	pid->outLimMin = outLimMin_val;
-	pid->T = T_val;
-	pid->moveSpeed = moveSpeed_val;
+		pid->outLimMax = outLimMax_val;
+		pid->outLimMin = outLimMin_val;
+		pid->T = T_val;
+		pid->moveSpeed = moveSpeed_val;
 
-	pid->integrator = 0;
-	pid->prevError = 0;
-	pid->prevMeasurement = 0;
+		pid->integrator = 0;
+		pid->prevError = 0;
+		pid->prevMeasurement = 0;
 }
 
 
@@ -83,33 +83,33 @@ void PID_Update(PID* pid, float setPoint, float measurement)
 	** Vitvlit Integratoris Mnishvnelobas
 	*/
 
-	// Vitvlit Sashualo Errors, Rac Aris Wina Da Axlandeli Errorebis Jamis Naxevari.
-	float averageError = 0.5 * (error + pid->prevError);
+		// Vitvlit Sashualo Errors, Rac Aris Wina Da Axlandeli Errorebis Jamis Naxevari.
+		float averageError = 0.5 * (error + pid->prevError);
 
-	// Sashualo Errors Vamravlebt Ganaxlebebs Shoris Droze. Anu Daaxloebit Vigebt Ra Errori Qonda 'T' Drois Ganmavlobashi.
-	float addition = averageError * pid->T;
+		// Sashualo Errors Vamravlebt Ganaxlebebs Shoris Droze. Anu Daaxloebit Vigebt Ra Errori Qonda 'T' Drois Ganmavlobashi.
+		float addition = averageError * pid->T;
 
-	// Migebuls Vamravlebt Ki-ze Da Vumatebt Integrators.
-	float integrator = pid->integrator + pid->Ki * addition;
+		// Migebuls Vamravlebt Ki-ze Da Vumatebt Integrators.
+		float integrator = pid->integrator +  pid->Ki * addition;
 
 
 	/*
 	** Vitvlit Derivative-s Mnishvnelobas
 	*/
 
-	// Axlandel Errors Vaklebt Wina errors, Da Vamravlebt Kd-ze.
-	float derivative = (error - pid->prevError) * pid->Kd;
+		// Axlandel Errors Vaklebt Wina errors, Da Vamravlebt Kd-ze.
+		float derivative = (error - pid->prevError) * pid->Kd;
 
-	// Vpiltravt Maqsimaluri Sixshiris Mixedvit
-	derivative = derivative * pid->Kn / (derivative + pid->Kn);
+		// Vpiltravt Maqsimaluri Sixshiris Mixedvit
+		derivative = derivative * pid->Kn / (derivative + pid->Kn);
 
 
 	/*
 	** Vitvlit Correction-s
 	*/
 
-	// Correction Aris Proporciulis, Integratoris Da Derivative-s Jami.
-	float Correction = proportional + integrator + derivative;
+		// Correction Aris Proporciulis, Integratoris Da Derivative-s Jami.
+		float Correction = proportional + integrator + derivative;
 
 
 	/* Vigebt Saboloo Pasuxs */
@@ -117,39 +117,39 @@ void PID_Update(PID* pid, float setPoint, float measurement)
 		// Anti-Windup.
 		// Tu Correction-i Maqsimalur Correction-is Mnishvnelobas Gascda Da Erroris Nishani Correctionis Nishanis Tolia Aseve Vclampavt Integrators.
 		// Amistvis Shemovigebt Axal Boolean-s.
-	bool clamp_integrator = false;
+		bool clamp_integrator = false;
 
-	// Vclampavt Correction-s Mis Maqsimalur Da Minimalur Mnishvnelobebs Shoris
-	if (Correction > pid->outLimMax - pid->moveSpeed)
-	{
-		Correction = pid->outLimMax - pid->moveSpeed;
+		// Vclampavt Correction-s Mis Maqsimalur Da Minimalur Mnishvnelobebs Shoris
+			if(Correction > pid->outLimMax - pid->moveSpeed)
+			{
+				Correction = pid->outLimMax - pid->moveSpeed;
 
-		if (sign(error) == sign(Correction)) // Tu Nishnebi Emtxveva, Vclampavt Integratorsac.
-			clamp_integrator = true;
-	}
+				if(sign(error) == sign(Correction)) // Tu Nishnebi Emtxveva, Vclampavt Integratorsac.
+					clamp_integrator = true;
+			}
 
-	else if (Correction < pid->outLimMin + pid->moveSpeed)
-	{
-		Correction = pid->outLimMin + pid->moveSpeed;
+			else if(Correction < pid->outLimMin + pid->moveSpeed)
+			{
+				Correction = pid->outLimMin + pid->moveSpeed;
 
-		if (sign(error) == sign(Correction)) // Tu Nishnebi Emtxveva, Vclampavt Integratorsac.
-			clamp_integrator = true;
-	}
+				if(sign(error) == sign(Correction)) // Tu Nishnebi Emtxveva, Vclampavt Integratorsac.
+					clamp_integrator = true;
+			}
 
-	//
+		//
 
-/*
-** Correctionis Dabrunebamde Vcvlit Shesacvlel Mnishvnelobebs.
-*/
+	/*
+	** Correctionis Dabrunebamde Vcvlit Shesacvlel Mnishvnelobebs.
+	*/
 
-// Wina Errorad Vsvavt Axlandel Errors.
-	pid->prevError = error;
+		// Wina Errorad Vsvavt Axlandel Errors.
+		pid->prevError = error;
 
-	// Wina Measurementad Vsvavt Axlandel Measurements.
-	pid->prevMeasurement = measurement;
+		// Wina Measurementad Vsvavt Axlandel Measurements.
+		pid->prevMeasurement = measurement;
 
-	if (!clamp_integrator)// Vcvlit Integrators, Tu Ar Wirdeba Anti-Windup.
-		pid->integrator = integrator;
+		if(!clamp_integrator)// Vcvlit Integrators, Tu Ar Wirdeba Anti-Windup.
+			pid->integrator = integrator;
 
 	// Vinaxavt Correction-s Struqturashi
 	pid->out = Correction;
